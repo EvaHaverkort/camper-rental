939eb5dd1f8815feabd9eed656e6f36810286721
amanda rojter <rojteramanda@gmail.com>
2022-08-23 12:12:26 +0100

    Created offers controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,35 @@
 class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def edit
+    @offer = Offer.find(params[:id])
+  end
+
+  def update
+    @offer = Offer.find(params[:id])
+  end
+
+  def destroy
+    @offer.destroy
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:name, :price)
+  end
 end
