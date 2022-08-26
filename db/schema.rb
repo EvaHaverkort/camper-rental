910fcb3371d30257739cbabfe61d9e89ad1a0fb9
Joana Neuenschwander <jo.neush@gmail.com>
2022-08-26 12:33:55 +0100

    Implement map to the index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_115739) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_101510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_115739) do
     t.integer "price"
     t.string "name"
     t.string "picture_url"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
